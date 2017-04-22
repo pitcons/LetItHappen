6794e11e6bd81788fac883aea38ecf367fd02204
pit <pit>
2017-04-22 14:56:52 +0300

    first commit

---
+++ Source/LetItHappen/LetItHappen.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "LetItHappen.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, LetItHappen, "LetItHappen" );
---
+++ Source/LetItHappen/LetItHappenGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "LetItHappenGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LETITHAPPEN_API ALetItHappenGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
